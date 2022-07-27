c591cef319cd67e37538a18c6925ab7231f504a3
surajjadhav04 <surajvjadhav22@gmail.com>
2022-07-27 09:29:13 +0530

    blog post and comments app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :views
+  resources :comments
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	# authenticated :user do
+ #  	root :to => "post#index"
+	# end
+
+	 devise_scope :user do
+  	root to: "devise/sessions#new"
+	end
 end
